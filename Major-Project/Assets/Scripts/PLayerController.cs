d945fd0741886fe8d214f3309f797bdfe911cb05
AnonymooseProf <sktconnor@gmail.com>
2022-03-18 00:16:38 +0000

    Added animation and player movement

---
+++ Major-Project/Assets/Scripts/PLayerController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float movementSpeed = 4f;
+    float movementLimter = 0.7f;
+    float inputVerticle;
+    float inputHorizontal;
+    Animator animator;
+    string currentState;
+    const string IDLE_DOWN = "idle_down";
+    const string IDLE_UP = "idle_up";
+    const string IDLE_SIDE = "idle_side";
+    const string RUN_UP = "run_up";
+    const string RUN_DOWN = "run_down";
+    const string RUN_SIDE = "run_side";
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        inputVerticle = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        if (inputHorizontal != 0 || inputVerticle != 0)
+        {
+            if (inputHorizontal != 0 && inputVerticle != 0)
+            {
+                inputHorizontal *= movementLimter;
+                inputVerticle *= movementLimter;
+            }
+
+            rb.velocity = new Vector2(inputHorizontal * movementSpeed, inputVerticle * movementSpeed);
+
+            if(inputVerticle > 0)
+            {
+                AnimationStateChanger(RUN_UP);
+            }
+            if (inputVerticle < 0)
+            {
+                AnimationStateChanger(RUN_DOWN);
+            }
+
+        }
+        else
+        {
+            rb.velocity = new Vector2(0f, 0f);
+            AnimationStateChanger(IDLE_DOWN);
+        }
+    }
+
+    void AnimationStateChanger(string newState)
+    {
+        if (currentState == newState) return;
+        animator.Play(newState);
+        currentState = newState;
+    }
+}
